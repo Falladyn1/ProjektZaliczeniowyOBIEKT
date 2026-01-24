d1cd4a1221f1d68a1dc2455c7b35cc2be2cb9760
Falladyn1 <matibati300@gmail.com>
2026-01-24 15:39:57 +0100

    p

--- ProjektZaliczeniowy/WagonBezprzedzialowy.cpp
+++ ProjektZaliczeniowy/WagonBezprzedzialowy.cpp
@@ -1,15 +1,2 @@
 #include "WagonBezprzedzialowy.h"
-#include <algorithm>
-wagonBezprzedzialowy::wagonBezprzedzialowy(int _nr, int _liczbaRzedow, bool czyStoliki)
-    : Wagon(_nr, czyStoliki), liczbaRzedow(_liczbaRzedow) {
-    generujMiejsca();
-}
 
-void wagonBezprzedzialowy::generujMiejsca() {
-    siedzenia.clear();
-
-    for (int r = 1; r <= liczbaRzedow; r++) {
-
-
-    }
-}
--- ProjektZaliczeniowy/WagonBezprzedzialowy.h
+++ ProjektZaliczeniowy/WagonBezprzedzialowy.h
@@ -5,7 +5,4 @@ class wagonBezprzedzialowy : public Wagon {
 private:
     int liczbaRzedow;
 public:
-    // Konstruktor przyjmuje tez informacje o stolikach
-    wagonBezprzedzialowy(int _nr, int _liczbaRzedow, bool czyStoliki);
-    void generujMiejsca() override;
 };
